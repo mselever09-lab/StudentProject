513cc749f91fef5846bf4e4c30aa156142281276
mselever09-lab <mselever09@gmail.com>
2026-05-27 16:14:25 +0300

    Fix DEBUG

--- webproject/settings.py
+++ webproject/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-spl^qowm+562g93#w)2-7oixa(c=^psw4!)nw4kz-@66@fc#lm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['edu-center-api.onrender.com', 'localhost', '127.0.0.1']
 
